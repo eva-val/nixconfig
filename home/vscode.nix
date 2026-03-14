68c332af43bdb94c8f7c5d40d16d579a72d94933
Eva Valentine <stephanie.howanietz@pm.me>
2026-03-14 13:12:36 -0700

    non depricated config

--- home/vscode.nix
+++ home/vscode.nix
@@ -12,45 +12,47 @@ in
   programs.vscode = {
     enable = true;
 
-    extensions =
-      with pkgs.vscode-extensions;
-      [
-        jnoortheen.nix-ide
-        mkhl.direnv
-        editorconfig.editorconfig
-        rust-lang.rust-analyzer
-      ]
-      ++ [ witchHazel ];
+    profiles.default = {
+      extensions =
+        with pkgs.vscode-extensions;
+        [
+          jnoortheen.nix-ide
+          mkhl.direnv
+          editorconfig.editorconfig
+          rust-lang.rust-analyzer
+        ]
+        ++ [ witchHazel ];
 
-    userSettings = {
-      "workbench.colorTheme" = "Witch Hazel";
+      userSettings = {
+        "workbench.colorTheme" = "Witch Hazel";
 
-      "editor.fontSize" = 14;
-      "editor.tabSize" = 2;
-      "editor.insertSpaces" = true;
-      "editor.formatOnSave" = true;
-      "editor.renderWhitespace" = "trailing";
+        "editor.fontSize" = 14;
+        "editor.tabSize" = 2;
+        "editor.insertSpaces" = true;
+        "editor.formatOnSave" = true;
+        "editor.renderWhitespace" = "trailing";
 
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.serverSettings" = {
-        nil = {
-          formatting = {
-            command = [ "nixfmt" ];
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.serverSettings" = {
+          nil = {
+            formatting = {
+              command = [ "nixfmt" ];
+            };
           };
         };
+
+        "files.associations" = {
+          "*.nix" = "nix";
+          "flake.lock" = "json";
+        };
+
+        "terminal.integrated.defaultProfile.linux" = "fish";
+
+        "files.trimTrailingWhitespace" = true;
+        "files.insertFinalNewline" = true;
+        "files.trimFinalNewlines" = true;
       };
-
-      "files.associations" = {
-        "*.nix" = "nix";
-        "flake.lock" = "json";
-      };
-
-      "terminal.integrated.defaultProfile.linux" = "fish";
-
-      "files.trimTrailingWhitespace" = true;
-      "files.insertFinalNewline" = true;
-      "files.trimFinalNewlines" = true;
     };
   };
 }
