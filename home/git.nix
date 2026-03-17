179ea46d10e710310523ffb25733ef54fdbad82b
Eva Valentine <stephanie.howanietz@pm.me>
2026-03-16 20:16:53 -0700

    gitconfig

--- home/git.nix
+++ home/git.nix
@@ -13,7 +13,38 @@
         email = "stephanie.howanietz@pm.me";
       };
       gpg.format = "ssh";
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
       init.defaultBranch = "main";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      core.excludesfile = "~/.gitignore";
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
     };
   };
 }
