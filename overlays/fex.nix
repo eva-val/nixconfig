f3f2d7084e5753b4275d0374a01665d0652b9455
Eva Valentine <stephanie.howanietz@pm.me>
2026-08-20 22:23:57 -0700

    flake

--- overlays/fex.nix
+++ overlays/fex.nix
@@ -1,16 +1,5 @@
 final: prev: {
-  # FEX's build scripts need packaging.version on Python 3.14. Remove this
-  # override once nixpkgs includes packaging in FEX's native build inputs.
-  fex = prev.fex.overrideAttrs (old: {
-    nativeBuildInputs = [
-      (final.python3.withPackages (
-        pythonPackages: with pythonPackages; [
-          setuptools
-          libclang
-          packaging
-        ]
-      ))
-    ]
-    ++ (old.nativeBuildInputs or [ ]);
-  });
+  # FEX 2605 doesn't build against fmt 12 (`fmt::join` over a span of bytes
+  # trips an undefined-template error). Remove once FEX supports fmt 12.
+  fex = prev.fex.override { fmt = prev.fmt_11; };
 }
