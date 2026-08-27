3c7eb04a8403d135d276f91c341df58672951c78
Eva Valentine <stephanie.howanietz@pm.me>
2026-08-26 23:00:01 -0700

    can: add scoped passwordless ip rules

--- modules/nixos/can.nix
+++ modules/nixos/can.nix
@@ -6,9 +6,26 @@
 }:
 
 let
+  canInterfaces = [
+    "can0"
+    "can1"
+  ];
+  ip = "/run/current-system/sw/bin/ip";
   configureInterface =
     interface:
     ''SUBSYSTEM=="net", ACTION=="add", KERNEL=="${interface}", RUN+="${pkgs.iproute2}/bin/ip link set ${interface} type can bitrate 500000 sample-point 0.8 dbitrate 2000000 dsample-point 0.8 fd on restart-ms 100", RUN+="${pkgs.iproute2}/bin/ip link set ${interface} up"'';
+  passwordlessCanCommands =
+    interface:
+    map
+      (command: {
+        inherit command;
+        options = [ "NOPASSWD" ];
+      })
+      [
+        "${ip} link set ${interface} up"
+        "${ip} link set ${interface} down"
+        "${ip} link set ${interface} type can *"
+      ];
 in
 {
   users.users.${hostSpec.username}.extraGroups = [ "dialout" ];
@@ -16,8 +33,14 @@ in
 
   # PEAK PCAN-USB Pro FD bench setup. Both ports remain CAN-FD so partial-
   # network wake frames work; classic diagnostic frames remain valid.
-  services.udev.extraRules = lib.concatMapStringsSep "\n" configureInterface [
-    "can0"
-    "can1"
+  services.udev.extraRules = lib.concatMapStringsSep "\n" configureInterface canInterfaces;
+
+  # Permit bench reconfiguration without granting unrestricted `ip`, whose
+  # namespace subcommands can execute arbitrary programs as root.
+  security.sudo.extraRules = [
+    {
+      users = [ hostSpec.username ];
+      commands = lib.concatMap passwordlessCanCommands canInterfaces;
+    }
   ];
 }
